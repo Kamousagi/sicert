34e3093ae4a79aa8348bf1cd35cb7dd132d251f3
Kamousagi <hector2c@live.com>
2018-05-09 01:18:45 -0500

    Se optimiza el control de errores al momento de validar y cargar la evaluación

--- app/Http/Controllers/CargarEvaluacionController.php
+++ app/Http/Controllers/CargarEvaluacionController.php
@@ -18,6 +18,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Validator;
+use App\Exceptions\NotFoundmonExceptio;
+use Exception;
 
 class CargarEvaluacionModelo {
     public $cod_evaluacion;
@@ -40,120 +42,149 @@ class CargarEvaluacionController extends Controller
 
     public function guardar(Request $solicitud) {
 
-        $messages = [
-            'cod_evaluacion.required' => 'Seleccione una evaluación',
-            'archivo.required' => 'Seleccione un archivo para cargar',
-        ];
-        
-        $validator = Validator::make($solicitud->all(), [
-            'cod_evaluacion' => 'required',
-            'archivo' => 'required',
-        ], $messages);
+        $errores = [];
+        try {
 
-        if ($validator->fails()) {
-            return redirect('/cargar_evaluacion')
-                        ->withErrors($validator)
-                        ->withInput();
-        }
-
-        $cod_evaluacion = $solicitud->input('cod_evaluacion');
-        $archivo = $solicitud->file('archivo');
-
-        $evaluacion = Evaluacion::with('detalle')->where('cod_evaluacion', $cod_evaluacion)->first();
-        $puntajes = Puntaje::all();
-        $instituciones = Institucion::with('ugel')->get();
-
-        $num_peso_total = $evaluacion->detalle->sum('num_peso');
-        DB::beginTransaction();
-
-        $evaluacion->ind_procesado = 1;
-        $evaluacion->save();
-
-        if ($solicitud->hasFile('archivo')) {
-
-            $data = file($archivo->getRealPath());
-            $indice = 0;
-
-            foreach($data as $linea) {
-
-                $indice++;
-                $nom_alumno = trim(substr($linea, 40, 14))." ".trim(substr($linea, 54, 12));
-                $num_seccion = substr($linea, 90, 2);
-                $num_institucion = substr($linea, 74, 7);
-                $nota = substr($linea, 92, 25);
-
-                if($nota == "*")
-                {
-                    DB::rollBack();
-                    die("no se encontro nota");
-                }
-
-                if($num_seccion == "" || $num_seccion == "**")
-                {
-                    die($indice.", No se encontro la sección en la fila x. : ".$nom_alumno);
-                }
+            $messages = [
+                'cod_evaluacion.required' => 'Seleccione una evaluación',
+                'archivo.required' => 'Seleccione un archivo para cargar',
+            ];
             
-                $institucion = $instituciones->where('num_institucion', $num_institucion)->first();
-                if($institucion == null)
-                {
-                    die($num_institucion);
-                }
-                $prueba = new Prueba();
-                $prueba->cod_institucion = $institucion->cod_institucion;
-                $prueba->nom_alumno = $nom_alumno;
-                $prueba->num_seccion = (int)$num_seccion;
-                $prueba->save();
-
-                $consolidado = new Consolidado();
-                $consolidado->cod_evaluacion = $cod_evaluacion;
-                $consolidado->nom_ugel = $institucion->ugel->nom_ugel;
-                $consolidado->nom_institucion = $institucion->nom_institucion;
-                $consolidado->num_peso_total = $num_peso_total;   //suma detalle evaluacion num_peso
-                $prueba->consolidado()->save($consolidado);
-
-                $num_nota = 0;
-                for($i=0; $i<25; $i++)
-                {
-                    $evaluacionDetalle = $evaluacion->detalle->where('num_pregunta', $i + 1)->first();
-                    $num_respuesta_marcada = (int)$nota[$i];
-                    if($evaluacionDetalle->num_respuesta == $num_respuesta_marcada)
-                    {
-                        $num_nota = (int)$num_nota + (int)$evaluacionDetalle->num_peso;
-                    }
-                }
-
-                $num_puntaje = ($num_nota * 100) / $num_peso_total;
-                $nom_comentario = $puntajes
-                    ->where('num_minimo', '<', $num_puntaje)
-                    ->where('num_maximo', '>=', $num_puntaje)
-                    ->first()
-                    ->nom_comentario;
-
-                $consolidadoCabeza = new ConsolidadoCabeza();
-                $consolidadoCabeza->nom_alumno = $nom_alumno;
-                $consolidadoCabeza->num_nota = $num_nota;   //25 pregunta , sumar las respuestas acertadas con el detalle de evaluacion <- si coinciden usar el peso del detalle de evaluacion
-                $consolidadoCabeza->nom_comentario = $nom_comentario; //de puntaje, calcular entre num_peso_total con num_nota (regla de 3)
-                $consolidadoCabeza->num_seccion = (int)$num_seccion;
-              
-                $consolidado->cabeza()->save($consolidadoCabeza);
-
-                for($i=1; $i<=25; $i++) {
-
-                    $pruebaDetalle = new PruebaDetalle();
-
-                    $evalucionDetalle = $evaluacion->detalle->where('num_pregunta', $i)->first();
-                    $pruebaDetalle->cod_evaluacion_detalle = $evalucionDetalle->cod_evaluacion_detalle;
-                    $pruebaDetalle->num_respuesta = (int)$nota[$i - 1];
-                    $prueba->detalle()->save($pruebaDetalle);
-
-                    $consolidadoCuerpo = new ConsolidadoCuerpo();
-                    $consolidadoCuerpo->num_pregunta = $i;
-                    $consolidadoCuerpo->num_respuesta = (int)$nota[$i - 1];
-                    $consolidadoCabeza->cuerpo()->save($consolidadoCuerpo);
-                }
-  
+            $validator = Validator::make($solicitud->all(), [
+                'cod_evaluacion' => 'required',
+                'archivo' => 'required',
+            ], $messages);
+    
+            if ($validator->fails()) {
+                return redirect('/cargar_evaluacion')
+                            ->withErrors($validator)
+                            ->withInput();
             }
+    
+            $cod_evaluacion = $solicitud->input('cod_evaluacion');
+            $archivo = $solicitud->file('archivo');
+    
+            $evaluacion = Evaluacion::with('detalle')->where('cod_evaluacion', $cod_evaluacion)->first();
+            $puntajes = Puntaje::all();
+            $instituciones = Institucion::with('ugel')->get();
+    
+            $num_peso_total = $evaluacion->detalle->sum('num_peso');
+            DB::beginTransaction();
+    
+            $evaluacion->ind_procesado = 1;
+            $evaluacion->save();
+    
+            if ($solicitud->hasFile('archivo')) {
+    
+                $data = file($archivo->getRealPath());
+                $indice = 0;
+    
+                $nom_alumno = [];
+                $num_seccion = [];
+                $num_institucion = [];
+                $nota = [];
 
+                foreach($data as $linea) {
+                    $indice++;
+                    $nom_alumno_valor = trim(substr($linea, 40, 14))." ".trim(substr($linea, 54, 12));
+                    $num_seccion_valor = substr($linea, 90, 2);
+                    $num_institucion_valor = substr($linea, 74, 7);
+                    $nota_valor = substr($linea, 92, 25);
+    
+                    if (strpos($nota_valor, '*') !== false)
+                    {
+                        $errores[] = "En la linea $indice, no se encontro una respuesta válida.";
+                    }
+    
+                    if($num_seccion_valor == "" || $num_seccion_valor == "**")
+                    {
+                        $errores[] = "En la linea $indice, no se encontro una seccón válida.";
+                    }
+                
+                    $institucion = $instituciones->where('num_institucion', $num_institucion_valor)->first();
+                    if($institucion == null)
+                    {
+                        $errores[] = "En la linea $indice, no se encontro la institución con código $num_institucion_valor.";
+                    }
+
+                    $nom_alumno[] = $nom_alumno_valor;
+                    $num_seccion[] = $num_seccion_valor;
+                    $num_institucion[] = $num_institucion_valor;
+                    $nota[] = $nota_valor;
+                }
+
+                if(count($errores))
+                {
+                    throw new Exception("Se encontraron advertencias en el archivo");
+                }
+
+                for($i=0; $i<=$indice; $i++)
+                {
+                    $institucion = $instituciones->where('num_institucion', $num_institucion[$i])->first();
+
+                    $prueba = new Prueba();
+                    $prueba->cod_institucion = $institucion->cod_institucion;
+                    $prueba->nom_alumno = $nom_alumno[$i];
+                    $prueba->num_seccion = (int)$num_seccion[$i];
+                    $prueba->save();
+    
+                    $consolidado = new Consolidado();
+                    $consolidado->cod_evaluacion = $cod_evaluacion;
+                    $consolidado->nom_ugel = $institucion->ugel->nom_ugel;
+                    $consolidado->nom_institucion = $institucion->nom_institucion;
+                    $consolidado->num_peso_total = $num_peso_total;   //suma detalle evaluacion num_peso
+                    $prueba->consolidado()->save($consolidado);
+    
+                    $num_nota = 0;
+                    for($j=0; $j<25; $j++)
+                    {
+                        $evaluacionDetalle = $evaluacion->detalle->where('num_pregunta', $j + 1)->first();
+                        $num_respuesta_marcada = (int)$nota[$i][$j];
+                        if($evaluacionDetalle->num_respuesta == $num_respuesta_marcada)
+                        {
+                            $num_nota = (int)$num_nota + (int)$evaluacionDetalle->num_peso;
+                        }
+                    }
+    
+                    $num_puntaje = ($num_nota * 100) / $num_peso_total;
+                    $nom_comentario = $puntajes
+                        ->where('num_minimo', '<', $num_puntaje)
+                        ->where('num_maximo', '>=', $num_puntaje)
+                        ->first()
+                        ->nom_comentario;
+    
+                    $consolidadoCabeza = new ConsolidadoCabeza();
+                    $consolidadoCabeza->nom_alumno = $nom_alumno;
+                    $consolidadoCabeza->num_nota = $num_nota;   //25 pregunta , sumar las respuestas acertadas con el detalle de evaluacion <- si coinciden usar el peso del detalle de evaluacion
+                    $consolidadoCabeza->nom_comentario = $nom_comentario; //de puntaje, calcular entre num_peso_total con num_nota (regla de 3)
+                    $consolidadoCabeza->num_seccion = (int)$num_seccion;
+                  
+                    $consolidado->cabeza()->save($consolidadoCabeza);
+    
+                    for($i=1; $i<=25; $i++) {
+    
+                        $pruebaDetalle = new PruebaDetalle();
+    
+                        $evalucionDetalle = $evaluacion->detalle->where('num_pregunta', $i)->first();
+                        $pruebaDetalle->cod_evaluacion_detalle = $evalucionDetalle->cod_evaluacion_detalle;
+                        $pruebaDetalle->num_respuesta = (int)$nota[$i - 1];
+                        $prueba->detalle()->save($pruebaDetalle);
+    
+                        $consolidadoCuerpo = new ConsolidadoCuerpo();
+                        $consolidadoCuerpo->num_pregunta = $i;
+                        $consolidadoCuerpo->num_respuesta = (int)$nota[$i - 1];
+                        $consolidadoCabeza->cuerpo()->save($consolidadoCuerpo);
+                    }
+      
+                }
+    
+            }
+            
+        }
+        catch(Exception $e)
+        {
+            DB::rollBack();
+            return redirect('/cargar_evaluacion')->withErrors(["errores" => $errores])->withInput();
         }
 
         DB::commit();
